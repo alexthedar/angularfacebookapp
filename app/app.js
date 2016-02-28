e27a69971b99b3aa8d95d960b5438e3a8b09aaf8
alexthedar <alex.castaneda@gmail.com>
2016-02-28 15:03:59 -0800

    fix error not calling ngfacebook.js

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('AngFBApp', [
   'ngRoute',
-  'AngFBApp.view1',
-  'AngFBApp.view2',
   'AngFBApp.facebook'
 ]).
 config(['$routeProvider', function($routeProvider) {
